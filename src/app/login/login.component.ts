087b688ccbc554bd88cdeaf402f929a5bf4870fc
gekikara404 <gekikara404@gmail.com>
2024-03-01 15:27:25 +0700

    init

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterOutlet, CardModule, ButtonModule, InputTextModule, CheckboxModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  username: string | undefined;
+}
