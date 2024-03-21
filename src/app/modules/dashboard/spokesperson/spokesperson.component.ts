886301b6ffd291b6fab01e97e87073ff422c0e49
gekikara404 <gekikara404@gmail.com>
2024-03-21 21:40:48 +0700

    init spokesperson

---
+++ src/app/modules/dashboard/spokesperson/spokesperson.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spokesperson',
+  standalone: true,
+  imports: [],
+  templateUrl: './spokesperson.component.html',
+  styleUrl: './spokesperson.component.scss'
+})
+export class SpokespersonComponent {
+
+}
