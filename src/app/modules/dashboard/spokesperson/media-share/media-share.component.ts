23f17c81d15e2dad858b5991b568d4dd4769c37e
gekikara404 <gekikara404@gmail.com>
2024-03-22 22:14:01 +0700

    implemented media share layout

--- src/app/modules/dashboard/spokesperson/media-share/media-share.component.ts
+++ src/app/modules/dashboard/spokesperson/media-share/media-share.component.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { htmlLegendPlugin } from '../../../../shared/utils/ChartUtils';
 
 @Component({
   selector: 'app-media-share',
   standalone: true,
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './media-share.component.html',
-  styleUrl: './media-share.component.scss'
+  styleUrl: './media-share.component.scss',
 })
 export class MediaShareComponent {
+  data: any;
+  options: any;
+  plugins = [htmlLegendPlugin];
 
+  ngOnInit() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    this.data = {
+      labels: [
+        'Faskes Kesehatan',
+        'Faskes Kesehatan',
+        'Faskes Kesehatan',
+        'Faskes Kesehatan',
+        'Faskes Kesehatan',
+        'Faskes Kesehatan',
+        'Faskes Kesehatan',
+      ],
+      datasets: [
+        {
+          data: [540, 325, 702, 702, 702, 702, 702],
+          percentages: [20, 20, 20, 20, 20, 20, 20],
+          backgroundColor: [
+            documentStyle.getPropertyValue('--blue-500'),
+            documentStyle.getPropertyValue('--yellow-500'),
+            documentStyle.getPropertyValue('--green-500'),
+          ],
+          hoverBackgroundColor: [
+            documentStyle.getPropertyValue('--blue-400'),
+            documentStyle.getPropertyValue('--yellow-400'),
+            documentStyle.getPropertyValue('--green-400'),
+          ],
+        },
+      ],
+    };
+
+    this.options = {
+      maintainAspectRatio: false,
+      plugins: {
+        htmlLegend: {
+          containerID: 'mediashare-legend-container',
+          flexDirection: 'row',
+          fontWeight: 'bold',
+          percentagesValueFontSize: '14px',
+        },
+        legend: {
+          display: false,
+          labels: {
+            usePointStyle: true,
+            color: textColor,
+          },
+        },
+      },
+    };
+  }
 }
