2560177998f22c2d4ad8e5699cd8ef810017b822
Chandra Sukmagalih Arifin <chandra.sukmagalih@bankmandiri.co.id>
2024-08-18 02:52:05 +0700

    update

--- src/app/core/components/highcharts/highcharts.component.ts
+++ src/app/core/components/highcharts/highcharts.component.ts
@@ -1,31 +1,48 @@
 import { NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { CommonService } from '../../services/common.service';
 import { SpinnerComponent } from '../spinner/spinner.component';
 import { Options } from 'highcharts';
-
 import darkTheme from 'highcharts/themes/dark-unica';
 import lightTheme from 'highcharts/themes/avocado';
+import { skip } from 'rxjs';
 
 @Component({
   selector: 'app-highcharts',
   standalone: true,
   imports: [NgIf, HighchartsChartModule, SpinnerComponent],
-  templateUrl: './highcharts.component.html',
-  styleUrl: './highcharts.component.scss',
+  template: `
+    <spinner [spinning]="isLoading">
+      <div class="app-card">
+        <div class="h-25rem" *ngIf="!data"></div>
+        <ng-container *ngIf="data">
+          <highcharts-chart
+            [Highcharts]="Highcharts"
+            [options]="data"
+            class="block w-full h-25rem"
+          ></highcharts-chart>
+        </ng-container>
+      </div>
+    </spinner>
+  `,
 })
-export class HighchartsComponent {
-  @Input() isLoading: boolean = false;
-  @Input() data!: Options;
-
+export class HighchartsComponent implements OnInit {
   Highcharts: typeof Highcharts = Highcharts;
 
-  constructor(public commonService: CommonService) {
-    commonService.darkMode$.subscribe((isDarkMode) => {
-      if (isDarkMode) darkTheme(Highcharts);
-      else lightTheme(Highcharts);
+  @Input() isLoading: boolean = false;
+  @Input() data?: Options;
+
+  constructor(public commonService: CommonService) {}
+
+  ngOnInit(): void {
+    this.commonService.isDarkMode
+      ? darkTheme(Highcharts)
+      : lightTheme(Highcharts);
+
+    this.commonService.darkMode$.pipe(skip(1)).subscribe(() => {
+      window.location.reload();
     });
   }
 }
